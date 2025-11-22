5469e741cd6d693e773fe3a5b6260734603b9b29
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-11-21 22:49:00 -0300

    Refactor: Update Spanish translations and enhance Slider functionality with new mathematical functions.

--- adf/nodes/slider.js
+++ adf/nodes/slider.js
@@ -1,4 +1,15 @@
-export default function create({ Node }) {
+export default function create({ Node, _ }) {
+  const functions =  [
+    { value: 'linear', label: _('Linear'), func: x => x, inverse: x => x },
+    { value: 'threeHalves', label: _('Three halves'), func: x => Math.pow(x, 3 / 2), inverse: x => Math.pow(x, 2 / 3) },
+    { value: 'quadratic', label: _('Quadratic'), func: x => x * x, inverse: x => Math.sqrt(x) },
+    { value: 'cubic', label: _('Cubic'), func: x => x * x * x, inverse: x => Math.pow(x, 1 / 3) },
+    { value: 'twoThirds', label: _('Two thirds'), func: x => Math.pow(x, 2 / 3), inverse: x => Math.pow(x, 3 / 2) },
+    { value: 'quadraticRoot', label: _('Quadratic Root'), func: x => Math.sqrt(x), inverse: x => x * x },
+    { value: 'cubicRoot', label: _('Cubic Root'), func: x => Math.pow(x, 1 / 3), inverse: x => x * x * x },
+    { value: 'sinusoidal', label: _('Sinusoidal'), func: x => (Math.sin((x - 0.5) * Math.PI) + 1) / 2, inverse: x => (Math.asin((x * 2) - 1) / Math.PI) + 0.5 },
+  ];
+
   return class Slider extends Node {
     shape = {
       shapes: [
@@ -9,6 +20,24 @@ export default function create({ Node }) {
           height: 1,
           rx: .1,
           ry: .1,
+        },
+        {
+          name: 'ticks',
+          shape: 'path',
+          x: .5,
+          d: `
+            M 0   -.3 V .3
+            M .5  -.3 V .3
+            M 1   -.3 V .3
+            M 1.5 -.3 V .3
+            M 2   -.3 V .3
+            M 2.5 -.3 V .3
+            M 3   -.3 V .3
+            M 3.5 -.3 V .3
+            M 4   -.3 V .3
+            M 4.5 -.3 V .3
+            M 5   -.3 V .3
+          `,
         },
         {
           shape: 'rect',
@@ -43,24 +72,46 @@ export default function create({ Node }) {
       { name: 'o0', type: 'out', x: 6, y: 0, direction: 'right', extends: 'tiny' },
     ];
 
-    formDefinition = [
-      {
-        name: 'size',
-        type: 'number',
-        _label: 'Size',
-      },
-      {
-        name: 'status',
-        type: 'number',
-        _label: 'Status',
-        min: this.min,
-        max: this.max,
-      },
-    ];
+    get formDefinition() {
+      return [
+        {
+          name: 'size',
+          type: 'number',
+          _label: 'Size',
+        },
+        {
+          name: 'min',
+          type: 'number',
+          _label: 'Minimum',
+          step: 0,
+        },
+        {
+          name: 'max',
+          type: 'number',
+          _label: 'Maximum',
+          step: 0,
+        },
+        {
+          name: 'status',
+          type: 'number',
+          _label: 'Status',
+          min: this.min,
+          max: this.max,
+          step: this.range / 100,
+        },
+        {
+          name: 'function',
+          type: 'select',
+          _label: 'Function',
+          options: functions,
+        },
+      ];
+    }
 
     #size = 6;
-    min = 0;
+    min = -1;
     max = 1;
+    #function = 'linear';
 
     get size() {
       return this.#size;
@@ -79,26 +130,67 @@ export default function create({ Node }) {
       return this.max - this.min;
     }
 
+    get function() {
+      this.#function = this.#function || 'linear';
+    }
+
+    set function(value) {
+      this.#function = value;
+      this.updateFunc();
+    }
+
+    updateFunc() {
+      const funcObj = functions.find(f => f.value === this.#function) || functions[0];
+      this.func = funcObj?.func || (x => x);
+      this.inv = funcObj?.inverse || (x => x);
+      this.update();
+    }
+
+    getData() {
+      const data = super.getData();
+      data.status = this.status;
+      data.function = this.#function;
+      data.size = this.size;
+      data.min = this.min;
+      data.max = this.max;
+      return data;
+    }
+
     statusUpdated() {
       this.updateKnob();
       super.statusUpdated();
     }
 
+    func = x => x;
+    inv = x => x;
+
     update() {
       super.update();
+
+      const ticksShape = this.shape.shapes.find(s => s.name === 'ticks');
+      if (ticksShape) {
+        const size = this.size - 1;
+        let path = '';
+        ticksShape.x = .5;
+        for (let i = 0; i <= 1; i += .1) {
+          path += `M ${this.func(i) * size} -.3 V .3 `;
+        }
+        ticksShape.d = path;
+      }
+
       this.updateKnob();
     }
 
     updateKnob() {
       const shape = this.shape.shapes.find(s => s.name === 'knob') ?? {};
       if (this.draggingKnob) {
-        shape.fill = 'lightblue';
+        shape.fill = '#808080';
       } else {
         shape.fill = null;
       }
 
-      shape.x = (this.status - this.min) / this.range * (this.size - 1) + .2;
-      this.actdia.tryUpdateShape(this, this.svgShape?.children?.[2], shape);
+      shape.x = (this.inv(this.status) - this.min) / this.range * (this.size - 1) + .2;
+      this.actdia?.tryUpdateShape(this, this.svgShape?.children?.[3], shape);
     }
 
     onMouseClick({ evt, item, shape }) {
@@ -161,8 +253,12 @@ export default function create({ Node }) {
 
       evt.preventDefault();
 
-      const delta = (mouse.x - this.draggingFrom.x - .2) / (this.size - 1);
-      let newStatus = this.min + delta * this.range;
+      let delta = (mouse.x - this.draggingFrom.x - .2) / (this.size - 1);
+      if (delta < 0)
+        delta = 0;
+      if (delta > 1)
+        delta = 1;
+      let newStatus = this.min + this.func(delta) * this.range;
       if (newStatus < this.min)
         newStatus = this.min;
       if (newStatus > this.max)
