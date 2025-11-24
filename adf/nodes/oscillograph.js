0a304afdf88b12942cd07ea29b091e84bd771c89
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-11-24 15:13:41 -0300

    Fix: Refactor Oscillograph shape and update rendering logic for status visualization

--- adf/nodes/oscillograph.js
+++ adf/nodes/oscillograph.js
@@ -22,6 +22,8 @@ export default function create({ Node }) {
           fill: 'black',
         },
         {
+          x: 0.5,
+          y: 0.5,
           shapes: [],
         }
       ],
@@ -41,9 +43,23 @@ export default function create({ Node }) {
     static skipMatrix = ['matrix'];
 
     matrix = [];
+    #height = 6;
+    #width = 20;
 
     constructor(params) {
       super(params);
+    }
+
+    update() {
+      this.box.width = this.#width;
+      this.box.height = this.#height;
+
+      this.shape.shapes[0].width = this.#width;
+      this.shape.shapes[0].height = this.#height;
+      this.shape.shapes[1].width = this.#width - 1;
+      this.shape.shapes[1].height = this.#height - 1;
+
+      super.update();
     }
 
     updateStatus() {
@@ -61,8 +77,8 @@ export default function create({ Node }) {
         svgElement.removeChild(svgElement.children[svgElement.children.length - 1]);
       }
 
-      const sy = 11 / status.length;
-      const ssy = sy / 2;
+      const sy = (this.#height - 1) / status.length;
+      const ssy = sy * .9;
       for (let i = 0; i < status.length; i++) {
         this.matrix[i] ??= [];
         this.matrix[i].push(status[i]);
@@ -71,38 +87,40 @@ export default function create({ Node }) {
         if (!shape) {
           this.shape.shapes[2].shapes[i] = {
             shape: 'path',
-            x: 0.5,
             fill: false,
             stroke: 'lime',
             strokeWidth: 1,
           };
           shape = this.shape.shapes[2].shapes[i];
         }
-        shape.y = 1 + i * sy;
+        shape.y = i * sy + .1;
 
-        let yy;
         shape.d = '';
         for (let k = 0; k < this.matrix[i].length; k++) {
-          const x = k * 0.2;
-          const y = (this.matrix[i][k] ?? 0) * ssy;
-          if (x > 11) {
+          let value = this.matrix[i][k];
+          if (value === true)
+            value = 1;
+          else if (value === false)
+            value = 0;
+
+          const x = k * 0.5;
+          const y = value * ssy;
+          if (x > this.#width - 1) {
             this.matrix[i].shift();
             break;
           }
 
           if (k === 0) {
-            shape.d += 'M';
+            shape.d += `M ${x} ${y}`;
           } else {
-            shape.d += `L ${x} ${yy} L`;
+            shape.d += ` H ${x} V ${y}`;
           }
-          shape.d += `${x} ${y} `;
-          yy = y;
         }
 
-        if (this.svgShape?.children?.[i]) {
+        if (this.svgShape?.children?.[1].children?.[i]) {
           this.actdia.tryUpdateShape(
             this,
-            svgElement?.children?.[i],
+            svgElement?.children?.[1].children?.[i],
             shape
           );
         } else {
