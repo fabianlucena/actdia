9e4c46a202d25050707ac93cad154839ad6f4d0f
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-11-26 14:10:29 -0300

    add src/actdia_interactive.js

--- adf/locale/es.js
+++ adf/locale/es.js
@@ -1,4 +1,2 @@
 export default {
-  'Can\'t load the file: %s.': 'No se puede cargar el archivo: %s.',
-  'Example diagram loaded.': 'Diagrama de ejemplo cargado.',
 };
---
+++ adf/src/actdia_interactive.js
@@ -0,0 +1,134 @@
+import ActDia from '../actdia/actdia.js';
+import NodeSelector from './node_selector.js';
+import NodeForm from './node_form.js';
+import { _, loadLocale } from '../actdia/locale.js';
+import { pushNotification, container as notificationContainer } from './notistack.js';
+import './drag.js';
+import { getPath } from '../actdia/utils.js';
+import ActDiaTools from './actdia_tools.js';
+
+let container = null,
+  actdia = null,
+  nodeSelector = null,
+  mouseSelectOn;
+
+window.addEventListener('beforeprint', () => notificationContainer.style.display = 'none');
+window.addEventListener('afterprint', () => notificationContainer.style.display = '');
+
+window.addEventListener('DOMContentLoaded', async () => {
+  await loadLocale(getPath(import.meta.url), 'es');
+
+  container = document.querySelector('#actdia');
+  actdia = new ActDia({ container, editable: true });
+  await actdia.init();
+
+  nodeSelector = new NodeSelector({ actdia, container });
+  nodeSelector.onSelectNode = onSelectNode;
+  nodeSelector.onSelectExample = onSelectExample;
+
+  actdia.onPushNotification = pushNotification;
+
+  new ActDiaTools({ container, actdia });
+
+  actdia.addEventListener('dblclick', dblClickHandler);
+  actdia.addEventListener('item:dblclick', itemDblClickHandler);
+  actdia.addEventListener('keydown', keyDownHandler, true);
+  actdia.addEventListener('keyup', keyUpHandler);
+
+  actdia.pushNotification(_('Welcome to ActDia!'), 'info');
+  if (window.location.hash) {
+    const data = JSON.parse(decodeURIComponent(window.location.hash.substring(1)));
+    actdia.load(data);
+  } else if (localStorage.getItem('actdia')) {
+    actdia.load(JSON.parse(localStorage.getItem('actdia')));
+  }
+});
+
+function dblClickHandler(evt) {
+  if (!actdia.editable)
+    return;
+
+  mouseSelectOn = actdia.getUntransformedPosition(actdia.mouse);
+  nodeSelector.show({ defaultPath: '../nodes' });
+}
+
+function itemDblClickHandler(evt) {
+  const { item } = evt.detail;
+  if (item) {
+    const { mouse } = evt.detail;
+    const nodeForm = new NodeForm({ container });
+    nodeForm.showForNode(item, { x: mouse.x, y: mouse.y });
+    evt.preventDefault();
+    evt.stopPropagation();
+  }
+}
+
+function onSelectNode({ evt, fqcn, }) {
+  evt.stopPropagation();
+  evt.preventDefault();
+
+  const { x, y } = mouseSelectOn;
+  actdia.addItem({
+    fqcn,
+    x,
+    y,
+  });
+
+  nodeSelector.close();
+}
+
+async function onSelectExample({ evt, url }) {
+  evt.stopPropagation();
+  evt.preventDefault();
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok)
+      throw new Error(_('Can\'t load the file: %s.', url));
+
+    const json = await response.json();
+    await this.actdia.load(json, { skipNotification: true });
+    pushNotification(_('Diagram loaded from JSON file.'), 'success');
+  } catch (err) {
+    console.error(err);
+    pushNotification(_('Invalid JSON file.'), 'error');
+  }
+  
+  nodeSelector.close();
+}
+
+function keyDownHandler(evt) {
+  switch (evt.key) {
+    case 'Escape': return escapeDefaultHandler(evt);
+    case 'Delete': return deleteDefaultHandler(evt);
+    case 'Backspace': return backspaceDefaultHandler(evt);
+  }
+}
+
+function escapeDefaultHandler(evt) {
+  actdia.cancelCaptureItem();
+  actdia.cancelDrag();
+}
+
+function deleteDefaultHandler(evt) {
+  actdia.deleteSelected();
+}
+
+function backspaceDefaultHandler(evt) {
+  actdia.deleteSelected();
+}
+
+function keyUpHandler(evt) {
+  switch (evt.key) {
+    case 'c': if (evt.ctrlKey || evt.metaKey)
+      copyJSONToClipboard(evt);
+  }
+}
+
+async function copyJSONToClipboard(options) {
+  const exportable = actdia.getExportableItems({ selected: true, ...options });
+  const jsonText = JSON.stringify(actdia.getData(exportable), null, 2);
+  const json = new ClipboardItem({ 'text/plain': new Blob([jsonText], { type: 'application/json' })});
+  await navigator.clipboard.write([json]);
+  pushNotification(_('JSON data copied to the clipboard.'), 'success');
+}
