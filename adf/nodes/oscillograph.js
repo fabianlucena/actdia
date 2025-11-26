28ff00e639033df068455e0386f5d6f19e5bd783
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-11-25 22:43:55 -0300

    Fix: Adjust scaleX value and implement interval for continuous drawing in Oscillograph

--- adf/nodes/oscillograph.js
+++ adf/nodes/oscillograph.js
@@ -42,11 +42,12 @@ export default function create({ Node }) {
 
     #height = 6;
     #width = 20;
-    #scaleX = .2;
+    #scaleX = .1;
     #matrix = [];
     #min = [];
     #max = [];
     #range = [];
+    #interval = null;
 
     get height() {
       return this.#height;
@@ -88,6 +89,15 @@ export default function create({ Node }) {
       this.shape.shapes[1].width = this.#width - 1;
       this.shape.shapes[1].height = this.#height - 1;
 
+      if (this.#interval) {
+        clearInterval(this.#interval);
+        this.#interval = null;
+      }
+
+      this.#interval = setInterval(() => {
+        this.draw();
+      }, 20);
+
       super.update();
     }
 
@@ -105,9 +115,10 @@ export default function create({ Node }) {
       while (svgElement?.children?.length > status.length) {
         svgElement.removeChild(svgElement.children[svgElement.children.length - 1]);
       }
+      
+      let length = this.#matrix[0]?.length || 0;
+      let last = length - 1;
 
-      const sy = (this.#height - 1) / status.length;
-      const ssy = sy * .9;
       for (let i = 0; i < status.length; i++) {
         let value = status[i];
         if (value === true)
@@ -117,8 +128,8 @@ export default function create({ Node }) {
         else if (isNaN(value))
           value = 0;
 
-        this.#matrix[i] ??= [];
-        this.#matrix[i].push(value);
+        this.#matrix[i] ??= Array(length).fill(0);
+        this.#matrix[i][last] = value ?? this.#matrix[i][last - 1] ?? 0;
         
         this.#min[i] = Math.min(this.#min[i] ?? value, value);
         this.#max[i] = Math.max(this.#max[i] ?? value, value);
@@ -130,14 +141,47 @@ export default function create({ Node }) {
             shape: 'path',
             fill: false,
             stroke: 'lime',
-            strokeWidth: 1,
+          };
+          shape = this.shape.shapes[2].shapes[i];
+        }
+      }
+    }
+
+    draw() {
+      const count = this.#matrix.length;
+      let length = this.#matrix[0]?.length || 0;
+      let last = length - 1;
+      const sy = (this.#height - 1) / count;
+      const ssy = sy * .9;
+      const svgElement = this.svgShape?.children?.[2];
+
+      if (length === 0) {
+        for (let i = 0; i < count; i++) {
+          this.#matrix[i] = [0];
+        }
+      } else {
+        for (let i = 0; i < count; i++) {
+          this.#matrix[i].push(this.#matrix[i][last]);
+        }
+      }
+
+      length++;
+      last++;
+
+      for (let i = 0; i < count; i++) {
+        let shape = this.shape.shapes[2].shapes[i];
+        if (!shape) {
+          this.shape.shapes[2].shapes[i] = {
+            shape: 'path',
+            fill: false,
+            stroke: 'lime',
           };
           shape = this.shape.shapes[2].shapes[i];
         }
         shape.y = i * sy + .1;
 
         shape.d = '';
-        for (let k = 0; k < this.#matrix[i].length; k++) {
+        for (let k = 0; k < length; k++) {
           let value = this.#matrix[i][k];
           const x = k * this.scaleX;
           const y = (1 - (value - this.#min[i]) / (this.#range[i] || 1 )) * ssy;
@@ -146,12 +190,10 @@ export default function create({ Node }) {
             break;
           }
 
-          if (k === 0) {
-            shape.d += `M ${x} ${y}`;
-          } else {
-            shape.d += ` H ${x} V ${y}`;
-          }
+          shape.d += ` L ${x} ${y}`;
         }
+
+        shape.d = 'M' + shape.d.substring(2);
 
         if (svgElement?.children?.[i]) {
           this.actdia.tryUpdateShape(
